704778b78a11258f5046d80bf35f8981e5382bd1
Nikhil2Nagpal <nikhilnagpal7519@gmail.com>
2025-09-21 15:53:57 +0530

    Update vite.config.js

--- creative-upaay-dashboard/vite.config.js
+++ creative-upaay-dashboard/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',  // 👈 Add this for safe deployment (avoid blank page)
+  base: './',  //  Add this for safe deployment (avoid blank page)
 })
